df770c16bf45ea4ef8b3832747133a3cbcc1b4d3
Victor Santiago <victorhsantiago3@gmail.com>
2020-03-14 15:33:12 -0300

    Add event card

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
     <h1>Events</h1>
+    <event-card
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+import EventCard from '~/components/EventCard.vue'
+
 export default {
+  components: { EventCard },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3001/events')
+      .then(({ data }) => ({ events: data }))
+      .catch((e) => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events this time. Please try again.',
+        })
+      })
+  },
   head() {
     return {
       title: 'Event Listing',
