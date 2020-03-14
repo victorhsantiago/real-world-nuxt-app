35c443bcd22e8e3ef7423f15429d00aee73a343f
Victor Santiago <victorhsantiago3@gmail.com>
2020-03-14 15:57:39 -0300

    Update eslint comma-daggle rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,21 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   extends: [
     '@nuxtjs',
     'prettier',
     'prettier/vue',
     'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
   ],
   plugins: ['prettier'],
   // add your custom rules here
   rules: {
-    'comma-dangle': ['error', 'always-multiline']
-  }
+    'comma-dangle': ['error', 'only-multiline'],
+  },
 }
