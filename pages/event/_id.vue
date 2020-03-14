6f7f62be768fc5e76221c94de6e9bcce56ed1a37
Victor Santiago <victorhsantiago3@gmail.com>
2020-03-14 15:58:23 -0300

    Add single event request

--- pages/event/_id.vue
+++ pages/event/_id.vue
@@ -1,24 +1,32 @@
 <template>
   <div>
-    <h1>Event #{{ id }}</h1>
+    <h1>{{ event.title }}</h1>
   </div>
 </template>
 
 <script>
 export default {
-  computed: {
-    id() {
-      return this.$route.params.id
-    },
+  async asyncData({ $axios, error, params }) {
+    try {
+      const { data } = await $axios.get(
+        `http://localhost:3001/events/${params.id}`
+      )
+      return { event: data }
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: `Unable to fetch event #${params.id}. Please try again.`,
+      })
+    }
   },
   head() {
     return {
-      title: `Event #${this.id}`,
+      title: this.event.title,
       meta: [
         {
           hid: 'description',
           name: 'description',
-          content: `What you need to know about event #${this.id}`,
+          content: `What you need to know about ${this.event.title}`,
         },
       ],
     }
